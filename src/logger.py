13e52269c851da3794d8076ca505771e7e41e422
Shruti Lad <ladshruti1405@gmail.com>
2026-09-22 01:00:09 +0530

    EDA and PROBLEM STATEMENT

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+from src.exception import CustomException
+
+LOG_FILE = f"log_{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
