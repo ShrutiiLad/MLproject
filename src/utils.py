747750cd48ed77d3f1d812064719784fc21ca0f0
Shruti Lad <ladshruti1405@gmail.com>
2026-09-23 17:03:26 +0530

    Model trainer done

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,31 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
-            
+
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+
+            # Train model
+            model.fit(X_train, y_train)
+             # Predict Testing data
+            y_train_pred = model.predict(X_train)
+           
+            y_test_pred = model.predict(X_test)
+
+            # Get r2 score for test data
+
+            train_model_score = r2_score(y_train, y_train_pred)    
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)   
